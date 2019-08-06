4351a33ab54fae081447f3403dbe0623b535026a
Glitch (filigree-playground) <none>
2019-08-06 19:35:04 +0000

    🍥🚉 Updated with Glitch: show structure; added insults

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-
-
-
+//=================================================================
+// SETTINGS
 
 // choose which grammar file to use
 
-let FILENAME = 'lovetester';
+//let FILENAME = 'insults';
 //let FILENAME = 'wizardSchool';
 //let FILENAME = 'bookreview';
 //let FILENAME = 'artreview';
-let N = 5;
+let FILENAME = 'lovetester';
+
+// how many messages to generate
+let N = 50;
+
+// if this is set, you'll get the same results every time.
+// set to null to get different results every time.
+let RANDOM_SEED = 'abc';
+
+// draw boxes around the rule structure?
+let SHOW_STRUCTURE = false;
 
 
 
 
 
-
+//=================================================================
+// SERVER CODE
 
 let fs = require('fs');
 let Filigree = require('filigree-text').Filigree;
@@ -23,22 +33,38 @@ let loadFil = () => {
   // read filigree file
   console.log('loading ' + FILENAME);
   let src = fs.readFileSync(`grammars/${FILENAME}.filigree`, 'utf-8');
-  let fil = new Filigree(src);
+  let fil = new Filigree(src, RANDOM_SEED);
   return fil;
 }
 
-let generate = (fil) => {
+// declare a couple of wrapper functions to use, depending on whather we want to show the structure
+
+// in this default version we neuter the angle brackets so we can see unreplaced rules in the output
+// and replace newlines with HTML linebreaks
+let doNotWrapperFn = (rule, text) =>
+  text
+  .split('<').join('&lt');
+
+// in this fancier wrapper function, we show the rule name in a box
+// we depend on CSS to handle newlines for us.
+// but we can't show leftover unmatched <rules> in the output because we can't
+// mess with their angle brackets without breaking the real HTML we're adding here.
+let fancyWrapperFn = (rule, text) =>
+  `<div class="container">
+    <sup class="ruleName">${rule}</sup>
+    <div class="output">${text}</div>
+  </div>`;
+
+// function to generate one message
+let generateOneMessage = (fil) => {
   if (fil.err) {
     // report error message
-    return '<div class="error">' + fil.err.message + '</div>';
+    return '<div class="error">' + fil.err.message.split('<').join('&lt;') + '</div>';
   }
+  
   // generate text
-  let text = fil.generate('start');
-  // sanitize angle-brackets so they don't count as HTML on the page
-  // this helps errors show up
-  text = text.split('<').join('&lt');
-  // convert newlines to HTML linebreaks
-  text = text.split('\n').join('<br/>');
+  let text = fil.generate('start', SHOW_STRUCTURE ? fancyWrapperFn : doNotWrapperFn);
+    
   return text;
 }
 
@@ -47,26 +73,30 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+// when the page is loaded...
 app.get('/', function(request, response) {
-  //response.sendFile(__dirname + '/views/index.html');
   // generate some texts
   let fil = loadFil();
   let texts = [];
   for (let ii = 0; ii < N; ii++) {
-    texts.push(generate(fil));
+    texts.push(generateOneMessage(fil));
   }
-  // put them in divs for css styling
+  
+  // put them each in divs for css styling
   let text = texts.map(t => '<div class="filigree">' + t + '</div>').join('\n');
+  
   // load html template from file
   let template = fs.readFileSync('views/index.html', 'utf-8');
+  
   // insert content into template
   let html = template.split('{{slot}}').join(text);
+
   // return it!
   response.set('Content-Type', 'text/html');
   response.send(html);
 });
 
-// go!
+// start the server
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
